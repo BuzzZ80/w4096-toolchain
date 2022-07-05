038a11bb5109e6db1e0cba363c06b03a7e5607f0
Buzz <BuzzWPendarvis@GMail.com>
2022-07-05 01:05:21 -0700

    added some comments in main

--- basm/basm-preprocessor/src/main.rs
+++ basm/basm-preprocessor/src/main.rs
@@ -4,6 +4,7 @@ mod lexer;
 mod parser;
 
 fn main() {
+    // Get input data
     let (filename, program) = match fileio::get_input() {
         Ok(s) => s,
         Err(e) => {
@@ -12,6 +13,7 @@ fn main() {
         }
     };
 
+    // Create lexer from input data and convert it into smaller parts for processing
     let mut lexer = lexer::Lexer::new(&filename, program);
 
     match lexer.tokenize() {
@@ -22,6 +24,7 @@ fn main() {
         }
     }
 
+    // Create parser from the output of the lexer, then process the data (resolve consts and includes, etc)
     let mut parser = parser::Parser::new(&filename, lexer.tokens.as_slice());
 
     match parser.parse() {
@@ -32,6 +35,20 @@ fn main() {
         }
     }
 
-    fileio::write_asm_file(&parser.output);
-    fileio::write_map_file(&parser.map);
+    // Write output to files
+    match fileio::write_asm_file(&parser.output){
+        Ok(()) => {}
+        Err(e) => {
+            println!("\x1b[95mBASM-PREPROCESSOR:\x1b[0m {}", e);
+            return;
+        }
+    }
+
+    match fileio::write_map_file(&parser.map) {
+        Ok(()) => {}
+        Err(e) => {
+            println!("\x1b[95mBASM-PREPROCESSOR:\x1b[0m {}", e);
+            return;
+        }
+    }
 }
